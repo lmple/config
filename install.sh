1cd3d0150bd2de671ccf61977b4e83af6e6e2389
Lièvre Loïc <llievre@protonmail.com>
2026-03-21 18:01:50 +0100

    feature: helix + tmux

--- install.sh
+++ install.sh
@@ -2,5 +2,8 @@
 ln -s "$(pwd)/zshrc" "$HOME/.zshrc"
 ln -s "$(pwd)/p10k.zsh" "$HOME/.p10k.zsh" # theme
 
+# tmux
+ln -s "$(pwd)/tmux.conf" "$HOME/.tmux.conf"
+
 # Helix
 ln -s "$(pwd)/helix" "$HOME/.config/"
