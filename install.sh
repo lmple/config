e803665fa12b7fa604f8100863413dbeb5a8c2f2
lmple <llievre@protonmail.com>
2026-04-12 09:26:01 +0200

    feat: wezterm

--- install.sh
+++ install.sh
@@ -7,6 +7,9 @@ ln -s "$(pwd)/starship.toml" "$HOME/.config/"
 # tmux
 ln -s "$(pwd)/tmux.conf" "$HOME/.tmux.conf"
 
+# Wezterm
+ln -s "$(pwd)/wezterm" "$HOME/.config"
+
 # Helix
 ln -s "$(pwd)/helix" "$HOME/.config/"
 
