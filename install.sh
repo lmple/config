f85ab6d6749ff005da8bec34a39d9eb435696f28
lmple <llievre@protonmail.com>
2026-04-18 20:37:50 +0200

    feat: Remove wezterm

--- install.sh
+++ install.sh
@@ -7,9 +7,6 @@ ln -s "$(pwd)/starship.toml" "$HOME/.config/"
 # tmux
 ln -s "$(pwd)/tmux.conf" "$HOME/.tmux.conf"
 
-# Wezterm
-ln -s "$(pwd)/wezterm" "$HOME/.config"
-
 # Helix
 ln -s "$(pwd)/helix" "$HOME/.config/"
 
