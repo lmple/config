9ae81827e191ffd7d9c4c10625b434aa675fb42e
lmple <llievre@protonmail.com>
2026-09-18 15:46:41 +0200

    feat: added new themes

---
+++ nvim/lua/plugins/ashen.lua
@@ -0,0 +1,9 @@
+-- Ashen: a warm, muted colorscheme (red/orange, grayscale, hints of teal)
+-- https://codeberg.org/ficd/ashen.nvim
+
+---@type LazySpec
+return {
+  "ficcdaf/ashen.nvim",
+  lazy = false,
+  priority = 1000,
+}
